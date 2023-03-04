faac448fefadd5193516997003b1bfb220aac154
Rafael Aldana <rafaelaldana6@Gmail.com>
2023-03-03 16:35:27 -0800

    added distributor

--- clients/distributor/index.js
+++ clients/distributor/index.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const handler = require('./handler');
+// const handler = require('./handler');
 const { socket } = require('../socket');
 
-setInterval (() => {
-  
-}, 5000);
 
-socket.on('', );
+socket.on('REQUEST_DELIVERY', (product) => {
+  socket.emit('PRODUCT_PICKED_UP', product);
 
-socket.emit('', );
+  console.log('Distributor: Product picked');
+  setTimeout(() => {
+    socket.emit('PRODUCT_DELIVERED', product);
+    console.log(`Distributor: ${product.productName} was delivered to the retailer.`);
+  }, 2000);
+});
+
